2e5bbbbae238e7318fb8ad3c4e82b6b987ad6c64
kwsong17 <159021511+kwsong17@users.noreply.github.com>
2024-02-18 17:58:30 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "rsuite/dist/rsuite.min.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
